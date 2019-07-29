483c447869defb230923aa36f378db04696809ef
Michael Haufe <tno@thenewobjective.com>
2019-07-29 03:20:57 -0500

    - updated licensing comment

--- src/invariant.ts
+++ src/invariant.ts
@@ -1,5 +1,6 @@
-/*!
- * Decorator Contracts v0.0.0 | Copyright (C) 2019 Michael L Haufe
+/**
+ * @license
+ * Copyright (C) 2019 Michael L Haufe
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
--- src/typings/Constructor.d.ts
+++ src/typings/Constructor.d.ts
@@ -1,1 +1,7 @@
+/**
+ * @license
+ * Copyright (C) 2019 Michael L Haufe
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
 type Constructor<T> = new(...args: any[]) => T;
