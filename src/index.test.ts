7d218c0e7e60c4b6b913312b2948203984df0e3b
Michael Haufe <tno@thenewobjective.com>
2019-09-29 12:23:45 -0500

    -Added Unit Test - Updated implementation - Implemented Unit Test

---
+++ src/index.test.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (C) #{YEAR}# Michael L Haufe
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Unit tests for the Contructor class
+ */
+import Contracts from './';
+
+/**
+ * Requirement 194
+ * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/194
+ */
+describe('', () => {
+    test('Construction', () => {
+        expect(new Contracts(true).debugMode).toBe(true);
+        expect(new Contracts(false).debugMode).toBe(false);
+    });
+});
