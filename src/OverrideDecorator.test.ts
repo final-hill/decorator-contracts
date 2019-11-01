526d9e2dd31033793a23f69350cd25abeac8d72d
Michael Haufe <tno@thenewobjective.com>
2019-11-01 07:06:43 -0500

    Requirement 341

--- src/OverrideDecorator.test.ts
+++ src/OverrideDecorator.test.ts
@@ -13,7 +13,7 @@ import { MSG_NO_MATCHING_MEMBER, MSG_INVALID_ARG_LENGTH, MSG_DUPLICATE_OVERRIDE 
  * Requirement 210
  * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/210
  */
-describe('The override decorator is a non-static method decorator only', () => {
+describe('The override decorator is a non-static member decorator only', () => {
     let {override} = new Contracts(true);
 
     test('class decorator throws', () => {
@@ -76,7 +76,7 @@ describe('In production mode the @override decorator is a no-op', () => {
  * Requirement 212
  * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/212
  */
-describe('Using @override on a method with no ancestor method is an error', () => {
+describe('Using @override on a class member with no ancestor member is an error', () => {
     let {override} = new Contracts(true);
 
     test('base class with @override decorator', () => {
@@ -191,7 +191,7 @@ describe('using @override on a method with an ancestor with a different paramete
  * Requirement 215
  * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/215
  */
-describe('A subclass with an overriding method missing @override is an error', () => {
+describe('A subclass with an overriding member missing @override is an error', () => {
     let {invariant, override} = new Contracts(true);
 
     test('@override defined', () => {
@@ -199,11 +199,16 @@ describe('A subclass with an overriding method missing @override is an error', (
             @invariant
             class Base {
                 method() {}
+
+                get foo() { return 3; }
             }
 
             class Sub extends Base {
                 @override
                 method() {}
+
+                @override
+                get foo() { return 4; }
             }
 
             return new Sub();
@@ -223,15 +228,27 @@ describe('A subclass with an overriding method missing @override is an error', (
 
             return new Sub();
         }).toThrow(`@override decorator missing on Sub.method`);
-    });
 
+        expect(() => {
+            @invariant
+            class Base {
+                get prop() { return 3; }
+            }
+
+            class Sub extends Base {
+                get prop() { return 5; }
+            }
+
+            return new Sub();
+        });
+    });
 });
 
 /**
  * Requirement 337
  * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/337
  */
-describe('Only a single @override can be assigned to a method per class', () => {
+describe('Only a single @override can be assigned to a member per class', () => {
     let {override} = new Contracts(true);
 
     test('duplicate @override', () => {
@@ -278,5 +295,53 @@ describe('Only a single @override can be assigned to a method per class', () => 
 
             return SubSub;
         }).not.toThrow();
+    });
+});
+
+/**
+ * Requirement 341
+ * https://dev.azure.com/thenewobjective/decorator-contracts/_workitems/edit/341
+ */
+describe('Accessors must support @override', () => {
+    let {invariant, override} = new Contracts(true);
+
+    test('instance accessor decorator does not throw', () => {
+        expect(() => {
+            @invariant
+            class Base {
+                private _value_: number = 0;
+                get value() { return this._value_; }
+                set value(x: number) { this._value_ = x; }
+            }
+
+            class Sub extends Base {
+                @override
+                set value(x: number) {
+                    super.value = x;
+                }
+            }
+
+            return Sub;
+        }).not.toThrow();
+    });
+
+    test('bad accessor decorator throws', () => {
+        expect(() => {
+            @invariant
+            class Base {
+                private _value_: number = 0;
+                get value() { return this._value_; }
+                set value(x: number) { this._value_ = x; }
+            }
+
+            class Sub extends Base {
+                @override
+                set foo(x: number) {
+                    super.value = x;
+                }
+            }
+
+            return Sub;
+        }).toThrow();
     });
 });
