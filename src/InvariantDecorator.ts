ecb1809dd7e08113b950113ad77c2ddc9c43ea83
Michael Haufe <tno@thenewobjective.com>
2019-11-24 21:28:49 -0600

    fixed file typo

--- src/InvariantDecorator.ts
+++ src/InvariantDecorator.ts
@@ -7,7 +7,7 @@
 import Assertion from './Assertion';
 import {ContractHandler, contractHandler} from './ContractHandler';
 import { OVERRIDE_LIST } from './OverrideDecorator';
-import isConstructor from './lib/isContructor';
+import isConstructor from './lib/isConstructor';
 import FnPredTable from './typings/FnPredTable';
 
 type ClassDecorator = <T extends Constructor<any>>(Constructor: T) => T;
