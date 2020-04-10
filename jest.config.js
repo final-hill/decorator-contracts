c667a49948e9f06fd67d2063fc4e0c2c21db2b12
Michael Haufe <tno@thenewobjective.com>
2020-04-09 20:55:18 -0500

    - Removed babel - moved clean tasks to commander script - updated configs to leverage typescript compiler - replaced parcel with webpack - updated dependencies

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,6 @@ module.exports = {
     ],
     "testEnvironment": "node",
     "transform": { 
-      '^.+\\.ts$': 'ts-jest',
-      '^.+\\.js$': 'babel-jest',
-    },
-    // https://github.com/facebook/jest/issues/8973#issuecomment-565116176
-    "globals": {
-      "ts-jest": {
-        "babelConfig": '.babelrc'
-      },
-      "babel-jest": {
-        "babelConfig": '.babelrc'
-      }
+      '^.+\\.ts$': 'ts-jest'
     }
 }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*!
+ * @license
+ * Copyright (C) #{YEAR}# Michael L Haufe
+ * SPDX-License-Identifier: AGPL-1.0-only
+*/
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.ts' ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
