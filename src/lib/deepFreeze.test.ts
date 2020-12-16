b936450312bd185bf2a2e26b18bea0aeb744f202
Michael Haufe <tno@thenewobjective.com>
2020-12-16 09:42:17 -0600

    Created deepFreeze unit test

---
+++ src/lib/deepFreeze.test.ts
@@ -0,0 +1,36 @@
+/*!
+ * @license
+ * Copyright (C) 2020 Final Hill LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * @see <https://spdx.org/licenses/AGPL-3.0-only.html>
+ */
+
+import deepFreeze from './deepFreeze';
+
+describe('deepFreeze testing', () => {
+    test('Shallow test', () => {
+        const obj = {
+            prop: 42
+        };
+
+        deepFreeze(obj);
+
+        expect(() => {
+            obj.prop = 33;
+        }).toThrow();
+    });
+
+    test('Deep test', () => {
+        const obj = {
+            prop1: {
+                prop2: 42
+            }
+        };
+
+        deepFreeze(obj);
+
+        expect(() => {
+            obj.prop1.prop2 = 33;
+        }).toThrow();
+    });
+});
